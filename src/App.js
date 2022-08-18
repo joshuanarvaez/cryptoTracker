1fb7bb418be5cb27e45c124eadbc639f1cd843eb
Josh Narvaez <jhnarva@gmail.com>
2022-08-18 15:08:14 -0500

    complete - need to add description of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Coin from './Coin';
+import './styles/app.css'
+
+
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data);
+        // console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="search-container">
+        <h1 className="search-header"> Top 100 Crypto Tracker </h1>
+        <form>
+          <input className="search-textbox" onChange={handleChange} type="text" placeholder="Search" />
+        </form>
+      </div>
+      <div className="header-columns">
+        <h2 className="col-1"> Mkt Cap Rank </h2>
+        <h2 className="col-2"> Symbol </h2>
+        <h2 className="col-3"> Coin </h2>
+        <h2 className="col-4"> Ticker </h2>
+        <h2 className="col-5"> Price </h2>
+        <h2 className="col-6"> 24h </h2>
+        <h2 className="col-7"> Market Cap </h2>
+        <h2 className="col-8"> Total Volume </h2>
+        <h2 className="col-9"> Circ. Supply </h2>
+        <h2 className="col-10"> ATH </h2>
+        <h2 className="col-11"> ATL </h2>
+        <h2 className="col-12"> Last Updated </h2>
+      </div>
+
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            symbol={coin.symbol}
+            name={coin.name}
+            image={coin.image}
+            price={coin.current_price}
+            marketCap={coin.market_cap}
+            marketCapRank={coin.market_cap_rank}
+            totalVolume={coin.total_volume}
+            high24h={coin.high_24h}
+            low24h={coin.low_24h}
+            priceChangePercentage24h={coin.price_change_percentage_24h}
+            marketCapChangePercentage24h={coin.market_cap_change_percentage_24h}
+            circulatingSupply={coin.circulating_supply}
+            totalSupply={coin.total_supply}
+            allTimeHigh={coin.ath}
+            allTimeLow={coin.atl}
+            lastUpdated={coin.last_updated}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import './styles/coin.css'
+
+
+const Coin = ({
+    symbol,
+    name,
+    image,
+    price,
+    marketCap,
+    marketCapRank,
+    totalVolume,
+    priceChangePercentage24h,
+    circulatingSupply,
+    allTimeHigh,
+    allTimeLow,
+    lastUpdated
+}) => {
+    return (
+        <>
+        <div className='data-columns'>
+            <div className="col-1">
+                <h5 className="market-cap-rank"> {marketCapRank} </h5>
+            </div>
+            
+            <div className="col-2">
+                <img src={image} alt="currency symbol" />
+            </div>
+
+            <div className="col-3">
+                <h5 className='coin-name'> {name} </h5>
+            </div>
+
+            <div className="col-4">
+                <h5 className="coin-ticker"> {symbol} </h5>
+            </div>
+
+            <div className="col-5">
+                <h5 className="coin-price"> {price.toLocaleString("en-US", { style: "currency", currency: "USD" })} </h5>
+            </div>
+
+            <div className="col-6">
+                {priceChangePercentage24h < 0 ? (
+                        <h5 className='coin-percent-24h red'>{priceChangePercentage24h.toFixed(2)}%</h5>
+                    ) : (
+                        <h5 className='coin-percent-24h green'>{priceChangePercentage24h.toFixed(2)}% </h5>
+                    )}
+            </div>
+
+            <div className="col-7">
+                <h5 className="coin-market-cap">{marketCap.toLocaleString("en-US", { style: "currency", currency: "USD" })} </h5>
+            </div>
+
+            <div className="col-8">
+                <h5 className="coin-total-volume">{totalVolume.toLocaleString("en-US", { style: "currency", currency: "USD" })} </h5>
+            </div>
+
+            <div className="col-9">
+                <h5 className="coin-circ-supply">{circulatingSupply.toLocaleString()} </h5>
+            </div>
+
+            <div className="col-10">
+                <h5 className="coin-ath">{allTimeHigh.toLocaleString("en-US", { style: "currency", currency: "USD" })} </h5>
+
+            </div>
+
+            <div className="col-11">
+                <h5 className="coin-atl">{allTimeLow.toLocaleString("en-US", { style: "currency", currency: "USD" })} </h5>
+
+            </div>
+
+            <div className="col-12">
+                <h5 className="coin-last-updated">{lastUpdated} </h5>
+            </div>
+        </div>
+        
+        
+        </>
+        
+
+    )
+}
+
+export default Coin
